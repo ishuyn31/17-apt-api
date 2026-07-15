e35f665c0fe4bfaed4cebb566841fd48e3fc63de
이서현 <ishuyn31@gmail.com>
2026-07-15 14:20:42 +0900

    gunicorn

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from scrapper import fetch_apt_trade
 
 app = Flask(__name__)
-app.config['TEMPLATES_AUTO_REROAD'] = True
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+@app.route("/search")
+def search():
+    DEAL_YMD = request.args.get("DEAL_YMD")
+    LAWD_CD = request.args.get("LAWD_CD")
+    results = fetch_apt_trade(LAWD_CD, DEAL_YMD)
+    return render_template("search.html", results=results)
 
 if __name__ == '__main__':
     app.run(debug=True)
